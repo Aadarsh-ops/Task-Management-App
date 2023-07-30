262aa566a32aa2d9d04cf73677d904681c31b36b
aadarsh-ops <paradisegamer649@gmail.com>
2023-07-30 23:41:05 +0530

    changes need for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBw0mL251DHl7hOdXyO_lwqysueiHKO5AE',
+    authDomain: 'task-app-3a874.firebaseapp.com',
+    projectId: 'task-app-3a874',
+    storageBucket: 'task-app-3a874.appspot.com',
+    messagingSenderId: '387262776023',
+    appId: '1:387262776023:web:7bb6ba2248a3b66b14e692',
+  },
 };
